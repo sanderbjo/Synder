e6d64d2fa6375ce2690e2aa80b7fd7aa24819b1a
bobauser <111920859+bobauser@users.noreply.github.com>
2023-10-23 16:24:06 +0200

    added-icons-and-fixed-design
    fixed the design of the Segmented button, only neccessary changes to make: center text vertically, increase space between icon and text
    Added 3 icons

--- app/src/main/java/com/example/synder/components/ChatsMatchesNavigation.kt
+++ app/src/main/java/com/example/synder/components/ChatsMatchesNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.synder.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -24,12 +25,21 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.synder.R
 
 @Composable
 fun SegmentedButton(curRoute: String, navController: NavHostController, checked: Number) {
@@ -37,12 +47,25 @@ fun SegmentedButton(curRoute: String, navController: NavHostController, checked:
     val activeColor = Color.Blue
     val inactiveColor = Color.Transparent // Transparent background for unselected button
 
+    val leftcard = RoundedCornerShape(
+        topStart = 20.dp,
+        topEnd =  0.dp,
+        bottomStart = 20.dp,
+        bottomEnd = 0.dp
+    )
+    val rightcard = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd =  20.dp,
+        bottomStart = 0.dp,
+        bottomEnd = 20.dp
+    )
+
     val icon1Color = if (checked == 1) Color.White else Color.Black
     val text1Color = if (checked == 1) Color.White else Color.Black
     val icon2Color = if (checked == 2) Color.White else Color.Black
     val text2Color = if (checked == 2) Color.White else Color.Black
 
-    val iconSize = 28.dp // Increase the icon size
+    val iconSize = 20.dp // Increase the icon size
     val buttonHeight = 40.dp // Increase the button height
 
     Row(
@@ -51,8 +74,7 @@ fun SegmentedButton(curRoute: String, navController: NavHostController, checked:
             .wrapContentSize(Alignment.Center)
             .padding(5.dp)
     ) {
-        // Chats Button
-        Row(
+        OutlinedCard(
             modifier = Modifier
                 .weight(1f) // Equal width for both buttons
                 .clickable {
@@ -65,23 +87,36 @@ fun SegmentedButton(curRoute: String, navController: NavHostController, checked:
                         }
                     }
                 }
-                .border(1.dp, Color.Gray, RoundedCornerShape(30.dp, 0.dp, 0.dp, 30.dp)) // Round the left side
-                .background(if (checked == 1) activeColor else inactiveColor)
-                .height(buttonHeight)
-                .padding(4.dp)
-                .padding(start = 6.dp) // Add some additional left padding
-        ) {
-            Icon(imageVector = Icons.Default.Email, contentDescription = null, modifier = Modifier.size(iconSize), tint = icon1Color)
-            Text(
-                text = "Chats",
-                color = text1Color,
-                fontWeight = if (checked == 1) FontWeight.Bold else FontWeight.Normal,
-                fontSize = 20.sp // Increase font size
+                .height(buttonHeight),
+            border = BorderStroke(1.dp, Color.Gray),
+            shape = leftcard,
+            colors = CardDefaults.cardColors(
+                containerColor = if (checked == 1) activeColor else inactiveColor,
+                contentColor = if (checked == 1) activeColor else inactiveColor,
             )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 5.dp, start = 5.dp),
+                horizontalArrangement = Arrangement.Center,
+            )  {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_chat_bubble_outline_24), // Replace R.drawable with your resource name
+                    contentDescription = null,
+                    modifier = Modifier.size(iconSize),
+                    tint = icon1Color,
+                )
+                Text(
+                    text = "Chats",
+                    color = text1Color,
+                    fontWeight = if (checked == 1) FontWeight.Bold else FontWeight.Normal,
+                    fontSize = 15.sp
+                )
+            }
         }
 
-        // Matches Button
-        Row(
+        // ANDRE CARD HER:
+        OutlinedCard(
             modifier = Modifier
                 .weight(1f) // Equal width for both buttons
                 .clickable {
@@ -94,9 +129,54 @@ fun SegmentedButton(curRoute: String, navController: NavHostController, checked:
                         }
                     }
                 }
+                .height(buttonHeight),
+            border = BorderStroke(1.dp, Color.Gray),
+            shape = rightcard,
+            colors = CardDefaults.cardColors(
+                containerColor = if (checked == 2) activeColor else inactiveColor,
+                contentColor = if (checked == 2) activeColor else inactiveColor,
+            )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 5.dp, start = 5.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_perm_identity_24), // Replace R.drawable with your resource name
+                    contentDescription = null,
+                    modifier = Modifier.size(iconSize),
+                    tint = icon2Color
+                )
+                Text(
+                    text = "Syndere",
+                    color = text2Color,
+                    fontWeight = if (checked == 2) FontWeight.Bold else FontWeight.Normal,
+                    fontSize = 15.sp,
+                )
+            }
+        }
+    }
+
+
+    /*
+
+        OutlinedCard(
+            modifier = Modifier
+                .weight(1f) // Equal width for both buttons
+                .clickable {
+                    if (curRoute != Screen.Chats.name) {
+                        navController.navigate(Screen.Chats.name) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                        }
+                    }
+                }
+                .height(buttonHeight)
                 .border(1.dp, Color.Gray, RoundedCornerShape(0.dp, 30.dp, 30.dp, 0.dp)) // Round the right side
                 .background(if (checked == 2) activeColor else inactiveColor)
-                .height(buttonHeight)
                 .padding(4.dp)
                 .padding(end = 6.dp) // Add some additional right padding
         ) {
@@ -105,8 +185,8 @@ fun SegmentedButton(curRoute: String, navController: NavHostController, checked:
                 text = "Syndere",
                 color = text2Color,
                 fontWeight = if (checked == 2) FontWeight.Bold else FontWeight.Normal,
-                fontSize = 20.sp // Increase font size
+                fontSize = 15.sp // Increase font size
             )
-        }
-    }
+        }*/
+
 }
